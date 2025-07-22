d35da105074e8d50532ca86dcd6513ac8bd25b81
Vanessa Naranjo <vanelau_94@hotmail.com>
2025-07-22 18:24:15 -0500

    Modificación proyecto 22072025

--- src/main/java/com/lvnr/demo/repaso22072025/service/PersonaService.java
+++ src/main/java/com/lvnr/demo/repaso22072025/service/PersonaService.java
@@ -3,7 +3,6 @@ package com.lvnr.demo.repaso22072025.service;
 import com.lvnr.demo.repaso22072025.dto.PersonaDto;
 
 public class PersonaService {
-    PersonaDto personaDto=new  PersonaDto();
 
     public PersonaDto crearPersona (int cedula, String nombre, String apellidos){
         PersonaDto personaDto = new PersonaDto();
@@ -11,11 +10,5 @@ public class PersonaService {
         personaDto.setNombre(nombre);
         personaDto.setApellidos(apellidos);
         return personaDto;
-
-
-
-
-
-
     }
 }
